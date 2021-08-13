5911fe55e76a00b47ef4a4903cb030f7e9a57e17
shinouta <shinouta226@gmail.com>
2021-08-13 14:30:12 +0900

    delete 3nd shinouta

--- HelloEveryone.py
+++ HelloEveryone.py
@@ -2,8 +2,4 @@ print("Hello, ooiaai424")
 print("Good Morning, kuri")
 print("Hello, shinouta")
 print("Hello, I am Kazuki.")
-<<<<<<< HEAD
-print("Hello, shinouta")
 print("Hello, awa")
-=======
->>>>>>> 2e7d6c8c1f0c17b5904c65f56598623ec257e066
